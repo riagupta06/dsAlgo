ae88bf85cfff84af656f7f57e5dc8195f2164f30
ria <ria.nov6@gmail.com>
2021-04-17 22:46:46 +0530

    hash map sort by values using java8

---
+++ src/main/java/HashMapSortByValues.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class HashMapSortByValues {
+
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("Ria", 2);
+        map.put("Rishabh", 1);
+        map.put("Kiran", 3);
+
+        //sort by values
+        List<Map.Entry<String, Integer>> collect = map.entrySet().stream().sorted(Comparator.comparingInt(value -> value.getValue())).collect(Collectors.toList());
+        System.out.println(collect);
+
+        //reverse sort
+        Comparator<Map.Entry<String, Integer>> reversed = getComparator().reversed();
+        List<Map.Entry<String, Integer>> collectList = map.entrySet().stream().sorted(reversed).collect(Collectors.toList());
+        System.out.println(collectList);
+    }
+
+    private static Comparator<Map.Entry<String, Integer>> getComparator() {
+        return Comparator.comparingInt(value -> value.getValue());
+    }
+
+}
